8f8b2d139f1085accb329c2acf3d422fb39737e4
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-23 19:01:33 +0200

    cm

--- src/srcipts/calendar/calendar.js
+++ src/srcipts/calendar/calendar.js
@@ -1,6 +1,24 @@
+export const generateNumbersRange = (from, to) => {
+    const result = [];
+    for (let i = from; i <= to; i++) {
+        result.push(i);
+    }
+    return result;
+}
+
 const generateDay = () => {
     // функция должна сгенерировать и вернуть разметку недели в виде строки
     // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
+    const days = document.querySelector('.calendar__day')
+    
+    const sells = generateNumbersRange(0,23)
+    .map(daySell => `
+    <div
+        class="calendar__day-sell" data-time="${daySell}"
+    ></div>
+    `).join('');
+
+    days.innerHTML = sells;
 };
 
 export const renderWeek = () => {
@@ -9,5 +27,16 @@ export const renderWeek = () => {
     // каждый день должен содержать в дата атрибуте порядковый номер для в месяце
     // какую неделю отображать - берем из storage
     // после того, как отрисовали всю сетку для отображаемой недели - добавляем события методом renderEvents
+    const week = document.querySelector('.calendar__week')
+    
+    const days = generateNumbersRange(1,7)
+    .map(calendarDay => `
+    <div
+        class="calendar__day" data-day="${calendarDay}"
+        ><div class="calendar__day-line"></div>${generateDay()}</div>
+    `).join('');
 
+    week.innerHTML = days;
 };
+
+renderWeek();
--- src/srcipts/calendar/timescale.js
+++ src/srcipts/calendar/timescale.js
@@ -1,4 +1,34 @@
+export const generateNumbersRange = (from, to) => {
+    const result = [];
+    for (let i = from; i <= to; i++) {
+        result.push(i);
+    }
+    return result;
+}
+
+export const getTime = () => {
+    const newTime = generateNumbersRange(0, 23)
+        .map(num => {
+                if ((num - 10) < 0) {
+                    return `0${num}:00`}
+                else {return `${num}:00`}
+            });
+    return newTime;
+}
 export const renderTimescale = () => {
     // ф-ция должна генерировать разметку для боковой шкалы времени (24 часа)
     // полученную разметку вставьте на страницу с помощью innerHTML
+    const timeScale = document.querySelector('.calendar__time-scale')
+    const timePolar = document.createElement('div');
+    timePolar.classList.add('calendar__time-scale-polar');
+    timePolar.innerHTML = 'GMT+02'
+    
+
+    const timeString = getTime()
+        .map(time => `<div class="calendar__time-scale-time" data-scale-time="${time}">${time}</div>`).join('');
+
+        timeScale.innerHTML = timeString;
+        timeScale.prepend(timePolar);
 };
+
+renderTimescale();
