f6f4a10b96010137d7149745eccd072528df37ab
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-03-26 17:51:58 +0200

    cm

--- src/srcipts/header/navigation.js
+++ src/srcipts/header/navigation.js
@@ -3,22 +3,42 @@ import { renderWeek } from '../calendar/calendar.js';
 import { renderRedLine } from '../calendar/redLine.js';
 import { renderHeader } from '../calendar/header.js';
 import { getStartOfWeek, getDisplayedMonth } from '../common/time.utils.js';
+// import { shmoment } from '../common/shmoment.js';
 
 const navElem = document.querySelector('.navigation');
 const displayedMonthElem = document.querySelector('.navigation__displayed-month');
 
+
+
+
 function renderCurrentMonth() {
     // отрисовать месяц, к которому относиться текущая неделя
     displayedMonthElem.innerHTML = `${getDisplayedMonth(getItem('displayedWeekStart'))}`
+    console.log(getItem('displayedWeekStart'));
 }
 
-const onChangeWeek = e => {
+const onChangeWeek = event => {
     // при переключении недели обновите displayedWeekStart в storage
     // и перерисуйте все необходимые элементы страницы (renderHeader, renderWeek, renderCurrentMonth, renderRedLine)
     const monday = getItem('displayedWeekStart');
-    const inWeek = new Date();
-    inWeek.setDate(monday.getDate()+7);
-    setItem('displayedWeekStart', inWeek)
+    const inWeek = new Date(monday);
+
+    const next = event.target.classList.contains('fa-chevron-right');
+    const prew = event.target.classList.contains('fa-chevron-left');
+    const today = event.target.classList.contains('navigation__today-btn');
+
+    if (next) {
+        inWeek.setDate(monday.getDate()+7);
+        setItem('displayedWeekStart', inWeek)
+    }
+    if (prew) {
+        inWeek.setDate(monday.getDate()-7);
+        setItem('displayedWeekStart', inWeek)
+    }
+    if (today) {
+        setItem('displayedWeekStart', getStartOfWeek(new Date()))
+    }
+    console.log(getItem('displayedWeekStart', inWeek))
     renderHeader();
     renderWeek();
     renderCurrentMonth();
